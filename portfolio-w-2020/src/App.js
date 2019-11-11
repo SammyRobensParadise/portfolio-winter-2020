757a8b3b6af9ed2b3ba6376250bbeff0bfb30bc5
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-11-10 20:37:11 -0800

    setup initial diamonds

--- portfolio-w-2020/src/App.js
+++ portfolio-w-2020/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-
+import React from 'react'
+import DiamondOne from './assets/common/diamondOne'
+import WrapperContainer from './assets/common/wrapper'
 function App() {
   return (
-    <div className="web-App">
+    <div className='web-App'>
+      <WrapperContainer>
+        <DiamondOne></DiamondOne>
+      </WrapperContainer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ portfolio-w-2020/src/assets/common/diamondOne.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+
+const DiamondOne = () => {
+    const setWith = window.innerWidth
+    const setHeight = setWith*(363/323)
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width={setWith} height={setHeight} viewBox='0 0 1938 2178'>
+      <defs>
+        <filter id='Union_2' x='0' y='0' width='1427' height='2178' filterUnits='userSpaceOnUse'>
+          <feOffset dy='3' input='SourceAlpha' />
+          <feGaussianBlur stdDeviation='3' result='blur' />
+          <feFlood flood-opacity='0.161' />
+          <feComposite operator='in' in2='blur' />
+          <feComposite in='SourceGraphic' />
+        </filter>
+        <filter id='Union_1' x='270' y='0' width='1668' height='2178' filterUnits='userSpaceOnUse'>
+          <feOffset dy='3' input='SourceAlpha' />
+          <feGaussianBlur stdDeviation='3' result='blur-2' />
+          <feFlood flood-opacity='0.161' />
+          <feComposite operator='in' in2='blur-2' />
+          <feComposite in='SourceGraphic' />
+        </filter>
+      </defs>
+      <g id='Component_1_1' data-name='Component 1 – 1' transform='translate(9 6)'>
+        <g class='cls-4' transform='matrix(1, 0, 0, 1, -9, -6)'>
+          <path
+            id='Union_2-2'
+            data-name='Union 2'
+            fill={'#203f8f'}
+            d='M0,1080H1409L704.5,2160Zm0,0L704.5,0,1409,1080Z'
+            transform='translate(9 6)'
+          />
+        </g>
+        <g class='cls-3' transform='matrix(1, 0, 0, 1, -9, -6)'>
+          <path
+            id='Union_1-2'
+            data-name='Union 1'
+            fill={'#1c67a6'}
+            d='M0,1080H1650L825,2160Zm0,0L825,0l825,1080Z'
+            transform='translate(279 6)'
+          />
+        </g>
+      </g>
+    </svg>
+  )
+}
+export default DiamondOne
