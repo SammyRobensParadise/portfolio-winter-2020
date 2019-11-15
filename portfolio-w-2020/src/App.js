92f3531878a9cb263cbd74053a6e76ced99f9502
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-11-14 22:14:18 -0800

    continued to setup redux

--- portfolio-w-2020/src/App.js
+++ portfolio-w-2020/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { simpleAction } from './actions/action'
 import DiamondOne from './assets/common/diamondOne'
 import WrapperContainer from './assets/common/wrapper'
 import LandingText from './assets/components/landingText'
@@ -6,20 +8,34 @@ import LandingContextText from './assets/components/landingContextText'
 import NavigationBar from './assets/components/navigationBar'
 import GoButton from './assets/components/goButton'
 import ContributionsSection from './assets/components/contributionsWrapper'
-import ContributionsText from './assets/components/contributionsText'
-function App() {
-  return (
-    <div className='web-App'>
-      <WrapperContainer>
-        <NavigationBar />
-        <DiamondOne />
-        <LandingText />
-        <LandingContextText />
-        <GoButton />
-        <ContributionsSection />
-      </WrapperContainer>
-    </div>
-  )
-}
 
-export default App
+class App extends React.PureComponent {
+  simpleAction = event => {
+    this.props.simpleAction(event)
+    console.log(this.props)
+  }
+  render() {
+    return (
+      <div className='web-App' onClick={this.simpleAction}>
+        <WrapperContainer>
+          <NavigationBar />
+          <DiamondOne />
+          <LandingText />
+          <LandingContextText />
+          <GoButton />
+          <ContributionsSection />
+        </WrapperContainer>
+      </div>
+    )
+  }
+}
+const mapStateToProps = state => ({
+  ...state
+})
+const mapDispatchToProps = dispatch => ({
+  simpleAction: event => dispatch(simpleAction(event))
+})
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
--- portfolio-w-2020/src/reducers/rootReducer.js
+++ portfolio-w-2020/src/reducers/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import simpleReducer from './simpleReducers';
+import simpleReducer from './reducers';
 export default combineReducers({
  simpleReducer
 });
