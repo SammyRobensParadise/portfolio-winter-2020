1ac63c53a5a8af32ecdfb336a37858c029e9b559
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-11-16 17:49:12 -0800

    redux init testing

--- portfolio-w-2020/src/reducers/reducers.js
+++ portfolio-w-2020/src/reducers/reducers.js
@@ -1,44 +1,17 @@
-
 import { combineReducers } from 'redux'
-import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from '../actions/action'
-const { SHOW_ALL } = VisibilityFilters
+import { TOGGLE_CONTRIBUTION_TEXT } from '../actions/action'
 
-function visibilityFilter(state = SHOW_ALL, action) {
+const Actions = (state = {}, action) => {
   switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return action.filter
+    case TOGGLE_CONTRIBUTION_TEXT:
+      debugger
+      return Object.assign(state,action.data)
     default:
       return state
   }
 }
-
-function todos(state = [], action) {
-  switch (action.type) {
-    case ADD_TODO:
-      return [
-        ...state,
-        {
-          text: action.text,
-          completed: false
-        }
-      ]
-    case TOGGLE_TODO:
-      return state.map((todo, index) => {
-        if (index === action.index) {
-          return Object.assign({}, todo, {
-            completed: !todo.completed
-          })
-        }
-        return todo
-      })
-    default:
-      return state
-  }
-}
-
-const todoApp = combineReducers({
-  visibilityFilter,
-  todos
+const AppReduce = combineReducers({
+  Actions
 })
 
-export default todoApp
+export default AppReduce
--- portfolio-w-2020/src/store.js
+++ portfolio-w-2020/src/store.js
@@ -1,9 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore } from 'redux';
+import index from '../src/reducers/index';
 export default function configureStore() {
  return createStore(
-  rootReducer,
-   applyMiddleware(thunk)
+  index,
  );
 }
