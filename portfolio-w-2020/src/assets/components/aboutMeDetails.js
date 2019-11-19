8b0ec8cc0cd8e66b64de0be21974b238c631184b
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-11-18 18:59:25 -0800

    added about me section

---
+++ portfolio-w-2020/src/assets/components/aboutMeDetails.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import styled from 'styled-components'
+import { connect } from 'react-redux'
+import { white, deepBlue } from '../colors/common-colors'
+
+const pos = window.innerHeight + 2900
+const AboutMeText = styled.div`
+  position: absolute;
+  top: ${pos}px;
+  left: 200px;
+`
+const AboutMeName = styled.div`
+&{
+line-height: 50px;
+position: absolute;
+font-family: impact-urw, sans-serif;
+font-weight: 400;
+font-style: normal;
+font-size: 100px;
+width 1000px;
+text-align: left;
+z-index: ${p => (p.hide || p.hide === false ? '1' : '90')};
+color: transparent;
+-webkit-text-stroke-width: 2px;
+-webkit-text-stroke-color: #fff;
+}
+`
+const AboutMeTitle = styled.div`
+top: 80px;
+font-family: montserrat, sans-serif;
+font-weight: 200;
+font-style: normal;
+font-size: 25px;
+width 1000px;
+text-align: left;
+letter-spacing: 0;
+z-index:  99;
+position: relative;
+color: ${white};
+line-height: 60px;
+text-shadow: 0px 2px 6px #2b2b2a;
+`
+const DescriptionFirst = styled.div`
+top: 80px;
+font-family: montserrat, sans-serif;
+font-weight: 200;
+font-style: normal;
+font-size: 25px;
+width 1000px;
+text-align: left;
+letter-spacing: 0;
+z-index:  99;
+position: relative;
+color: ${white};
+line-height: 60px;
+text-shadow: 0px 2px 6px #2d2d2a;
+`
+const DescriptionPara = styled.p`
+top: 80px;
+font-family: montserrat, sans-serif;
+font-weight: 200;
+font-style: normal;
+font-size: 25px;
+width 1000px;
+text-align: left;
+letter-spacing: 0;
+z-index:  99;
+position: relative;
+color: ${white};
+line-height: 60px;
+text-shadow: 0px 2px 6px #2d2d2a;
+`
+const SAMMY_ROBENS_PARADISE = 'SAMMY ROBENS-PARADISE'
+const WHAT_I_DO = 'I study Systems Design Engineering'
+const DESCRIPTION_FIRST = 'In other words, I study the way humans interact with technology.'
+const DESCRIPTION_PARA =
+  'Every day we interact with dozens of different systems, from the traffic system on your commute, to your email inbox - All of these systems play a role in our every day lives. At a macro level, I study the way that all of these vastly complex systems interact with their environment, whether that is humans, animals or other systems. Systems engineering combines all aspects of modern engineering to construct, visualize and improve the way we experience the world, and beyond.'
+class AboutMeDetails extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ...props
+    }
+  }
+  render() {
+    return (
+      <div>
+        <AboutMeText>
+          <AboutMeName>{SAMMY_ROBENS_PARADISE}</AboutMeName>
+          <AboutMeTitle>
+            <b>{WHAT_I_DO}</b>
+          </AboutMeTitle>
+          <DescriptionFirst>{DESCRIPTION_FIRST}</DescriptionFirst>
+          <DescriptionPara>{DESCRIPTION_PARA}</DescriptionPara>
+        </AboutMeText>
+      </div>
+    )
+  }
+}
+const mapStateToProps = state => {
+  return {
+    ...state
+  }
+}
+export default connect(mapStateToProps)(AboutMeDetails)
--- portfolio-w-2020/src/assets/components/aboutMeWrapper.js
+++ portfolio-w-2020/src/assets/components/aboutMeWrapper.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 import { white, deepBlue } from '../colors/common-colors'
-
+import AboutMeDetails from './aboutMeDetails'
 const pos = window.innerHeight + 2000
 const setWith = window.innerWidth - 50
 const setHeight = setWith * (1557 / 908.001)
@@ -21,43 +21,46 @@ class AboutMeWrapper extends React.PureComponent {
   }
   render() {
     return (
-      <AboutMeWrapperComp>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width={setWith}
-          height={setHeight}
-          viewBox='0 0 1565.999 907.001'
-        >
-          <defs>
-            <filter
-              id='Exclusion_21'
-              x='0'
-              y='0'
-              width='1565.999'
-              height='907.001'
-              filterUnits='userSpaceOnUse'
-            >
-              <feOffset dy='3' input='SourceAlpha' />
-              <feGaussianBlur stdDeviation='3' result='blur' />
-              <feFlood floodOpacity='0.161' />
-              <feComposite operator='in' in2='blur' />
-              <feComposite in='SourceGraphic' />
-            </filter>
-          </defs>
-          <g transform='matrix(1, 0, 0, 1, 0, 0)' filter='url(#Exclusion_21)'>
-            <path
-              id='Exclusion_21-2'
-              data-name='Exclusion 21'
-              d='M94.5,888H0V0H94.5V888ZM21.2,777.125v16.889l23.255,2.953-12.674,1.785c-4.063.554-7.623,1.081-10.581,1.569v16.765H71v-11.32H38.146L71,801v-8.029l-33.622-4.523H71v-11.32Zm0-26.793v21.595H71V749.471H61.033v9.5H50.081v-8.09H40.607v8.09H31.163v-8.644ZM1547,888H105.5V0H1547V888h0Z'
-              transform='translate(9.5 6.5)'
-              fill='#1d839c'
-              stroke='#1f66a6'
-              stroke-width='1'
-              filter='url(#Exclusion_21)'
-            />
-          </g>
-        </svg>
-      </AboutMeWrapperComp>
+      <div>
+        <AboutMeWrapperComp>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width={setWith}
+            height={setHeight}
+            viewBox='0 0 1565.999 907.001'
+          >
+            <defs>
+              <filter
+                id='Exclusion_21'
+                x='0'
+                y='0'
+                width='1565.999'
+                height='907.001'
+                filterUnits='userSpaceOnUse'
+              >
+                <feOffset dy='3' input='SourceAlpha' />
+                <feGaussianBlur stdDeviation='3' result='blur' />
+                <feFlood floodOpacity='0.161' />
+                <feComposite operator='in' in2='blur' />
+                <feComposite in='SourceGraphic' />
+              </filter>
+            </defs>
+            <g transform='matrix(1, 0, 0, 1, 0, 0)' filter='url(#Exclusion_21)'>
+              <path
+                id='Exclusion_21-2'
+                data-name='Exclusion 21'
+                d='M94.5,888H0V0H94.5V888ZM21.2,777.125v16.889l23.255,2.953-12.674,1.785c-4.063.554-7.623,1.081-10.581,1.569v16.765H71v-11.32H38.146L71,801v-8.029l-33.622-4.523H71v-11.32Zm0-26.793v21.595H71V749.471H61.033v9.5H50.081v-8.09H40.607v8.09H31.163v-8.644ZM1547,888H105.5V0H1547V888h0Z'
+                transform='translate(9.5 6.5)'
+                fill='#1d839c'
+                stroke='#1f66a6'
+                stroke-width='1'
+                filter='url(#Exclusion_21)'
+              />
+            </g>
+          </svg>
+        </AboutMeWrapperComp>
+        <AboutMeDetails />
+      </div>
     )
   }
 }
