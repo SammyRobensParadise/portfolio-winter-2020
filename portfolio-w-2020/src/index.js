6e079d5742e39346a72062020b3e95a8793b8502
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-12-15 15:49:36 -0800

    auto commit

--- portfolio-w-2020/src/index.js
+++ portfolio-w-2020/src/index.js
@@ -1,17 +1,21 @@
+/* eslint-disable import/no-named-as-default-member */
 import React from 'react'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import AppReduce from '../src/reducers/reducers'
+import './index.css'
+import { render } from 'react-dom'
+import * as serviceWorker from './serviceWorker'
+// eslint-disable-next-line import/no-named-as-default
+import App from './App'
+
+import AppReduce from './reducers/reducers'
+
 const store = createStore(AppReduce)
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
