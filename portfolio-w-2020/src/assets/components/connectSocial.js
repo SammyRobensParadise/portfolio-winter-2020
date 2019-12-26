d36c21397ba645f2ae89767b147641a5c9b2abe6
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-12-26 13:52:39 -0800

    began implementing sliding share animation for website connect button

--- portfolio-w-2020/src/assets/components/connectSocial.js
+++ portfolio-w-2020/src/assets/components/connectSocial.js
@@ -1,7 +1,27 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import styled, { keyframes, css } from 'styled-components'
 import { white, navGreen } from '../colors/common-colors'
+
+const moveOpen = keyframes`
+0%{
+height: 60px;
+}
+100%{
+height: 400px;
+}
+`
+const moveClose = keyframes`
+0%{
+height: 400px;
+}
+100%{
+height: 60px;
+}
+`
+const animOpenContainer = (anim) => css`
+  ${anim} 0.5s cubic-bezier(0.645, 0.045, 0.355, 1) 0s forwards;
+`
+
 const SocialConnectContainer = styled.div`
   @import url('https://css.gg/share.css');
   position: fixed;
@@ -9,6 +29,7 @@ const SocialConnectContainer = styled.div`
   right: 60px;
   z-index: 2000;
 `
+
 const SocialConnectCircle = styled.button`
   width: 60px;
   height: 60px;
@@ -18,6 +39,9 @@ const SocialConnectCircle = styled.button`
   stroke: ${(p) => (p.showingShareIconbar ? navGreen : white)};
   border-color: ${(p) => (p.showingShareIconbar ? navGreen : white)};
   box-shadow: 2px 10px 70px -8px rgba(0, 0, 0, 0.88);
+  &:focus {
+    outline: none;
+  }
 `
 const SocialConnectShare = styled.div`
   @import url('https://css.gg/share.css');
@@ -44,11 +68,17 @@ const ShareIconListContainer = styled.div`
   stroke: ${navGreen};
   border-color: ${navGreen};
   width: 60px;
-  height: 400px;
+  height: 30px;
   border-radius: 30px;
   transform: translateY(60px);
   z-index: 100;
-  display: ${(p) => (p.showingShareIconbar ? 'flex' : 'none')};
+  display: ${(p) => (p.showingShareIconbar || p.isClosingShareIconBar ? 'flex' : 'none')};
+  animation: ${(p) =>
+    p.showingShareIconbar && !p.isClosingShareIconBar
+      ? animOpenContainer(moveOpen)
+      : !p.showingShareIconbar && p.isClosingShareIconBar
+      ? animOpenContainer(moveClose)
+      : 'none'};
 `
 const ICON_NAMES = {
   shareIcon: 'gg-share',
@@ -59,9 +89,12 @@ const ICON_NAMES = {
  * @param {boolean} showingShareIconbar
  * @returns {JSX} JSX
  */
-const ShareIconList = ({ showingShareIconbar }) => {
+const ShareIconList = ({ showingShareIconbar, isClosingShareIconBar }) => {
   return (
-    <ShareIconListContainer showingShareIconbar={showingShareIconbar}>
+    <ShareIconListContainer
+      showingShareIconbar={showingShareIconbar}
+      isClosingShareIconBar={isClosingShareIconBar}
+    >
       <ShareIcon param={'https://css.gg/mail.css'} height={'100px'}>
         <i className={ICON_NAMES.mailIcon} />
       </ShareIcon>
@@ -78,6 +111,7 @@ class ConnectSocial extends React.PureComponent {
     this.state = {
       ...props,
       showingShareIconbar: false,
+      isClosingShareIconBar: false,
     }
     document.addEventListener('click', (e) => {
       this.closeShareIconBar(e)
@@ -85,17 +119,18 @@ class ConnectSocial extends React.PureComponent {
   }
   /**
    * shows the share Icon bar
-   * @param {} null
+   * @param {null} null
    */
   ShowShareIconBar = () => {
     this.setState({
       showingShareIconbar: true,
+      isClosingShareIconBar: false,
     })
   }
 
   /**
    * closes the shareIcon bar
-   * @param {event} e
+   * @param {e} event
    */
   closeShareIconBar = (e) => {
     const { showingShareIconbar } = this.state
@@ -104,19 +139,25 @@ class ConnectSocial extends React.PureComponent {
     if (showingShareIconbar && !isException) {
       this.setState({
         showingShareIconbar: false,
+        isClosingShareIconBar: true,
       })
     }
+    setTimeout(() => {
+      this.setState({
+        isClosingShareIconBar: false,
+      })
+    }, 500)
   }
   /**
    * detects if a node is a decendant of
    * another node
-   * @param {element} parent
-   * @param {element} child
+   * @param {parent} node
+   * @param {child} node
    */
   isDecendant = (parent, child) => {
     var node = child.parentNode
-    while (node != null) {
-      if (node == parent) {
+    while (node !== null) {
+      if (node === parent) {
         return true
       }
       node = node.parentNode
@@ -125,11 +166,15 @@ class ConnectSocial extends React.PureComponent {
   }
 
   render() {
-    const { showingShareIconbar } = this.state
+    const { showingShareIconbar, isClosingShareIconBar } = this.state
+    debugger
     return (
       <div>
         <SocialConnectContainer id={`social-connect-container`} onClick={this.ShowShareIconBar}>
-          <ShareIconList showingShareIconbar={showingShareIconbar} />
+          <ShareIconList
+            showingShareIconbar={showingShareIconbar}
+            isClosingShareIconBar={isClosingShareIconBar}
+          />
           <SocialConnectCircle showingShareIconbar={showingShareIconbar}>
             <SocialConnectShare>
               <i className={ICON_NAMES.shareIcon} />
